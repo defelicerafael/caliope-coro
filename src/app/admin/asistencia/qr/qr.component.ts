a92ed605be182ce61908c2f78802c82b2ffe71bf
defelicerafael <defelicerafael@gmail.com>
2025-01-31 10:28:50 -0300

    master

---
+++ src/app/admin/asistencia/qr/qr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr',
+  imports: [],
+  templateUrl: './qr.component.html',
+  styleUrl: './qr.component.css'
+})
+export class QrComponent {
+
+}
